ae952e57f22128ab7a64182fffae0c8d12b9a462
oscar02r <oscary.ramirez@gmail.com>
2020-05-12 16:50:36 +0200

    Adding model proyect

--- servidor/controllers/authCotroller.js
+++ servidor/controllers/authCotroller.js
@@ -1,20 +1,49 @@
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { validationResult } = require('express-validator');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const Usuario = require("../models/Usuario");
 
 exports.autenticarUsuario = async (req, res) => {
+  const errores = validationResult(req);
 
-      const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
 
-      if( !errores.isEmpty() ){
-         return res.status(400).json({ errores:errores.array() });
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({ msg: "El usuario no existe" });
+    }
+
+    const passCorrecto = await bcryptjs.compare(password, usuario.password);
+
+    if (!passCorrecto) {
+      res.status(400).json({ msg: "El password es incorrecto" });
+    }
+
+    const payload = {
+      usuario: {
+        id: usuario.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.SECRETA,
+      {
+        expiresIn: 3600, // Vence en una hora
+      },
+      (error, token) => {
+        if (error) throw error;
+        
+        res.json({ token });
       }
-
-      const { email, password } = req.body;
-
-      try {
-           
-      } catch (error) {
-          
-      }
-}
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
